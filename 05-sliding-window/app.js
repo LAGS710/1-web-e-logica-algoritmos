c61f91b65852bd12f84eee5bf24d77f62202a753
Luis Angel <luisgonzales2_@hotmail.com>
2025-04-01 20:49:21 -0600

    agregado de comentarios

--- 05-sliding-window/app.js
+++ 05-sliding-window/app.js
@@ -1,17 +1,23 @@
 function findLongestWord(text) {
+    // TODO: Dividir el texto en palabras y almacenarlas en una variable
     const words = text.split(" ")
 
     let longestWord = ''; 
     let aux;
+
+    // TODO: Recorrer el arreglo de palabras con un ciclo
     for (let i = 0 ; i < words.length; i++){
         aux = words[i]
+        // TODO: Comparar la longitud de la palabra actual con la más larga
         if(aux.length > longestWord.length){
+            // Actualizar la palabra más larga
             longestWord = aux;
         }
     }
+    TODO: Retornar la palabra más larga encontrada
     return longestWord;
 }
-
+// Ejemplo de uso
 const text = "JavaScript es un lenguaje de programación increíble para aprender.";
-
-console.log(findLongestWord(text)); 
+// TODO: Llama a la función y muestra el resultado
+console.log(findLongestWord(text));  // Resultado esperado: "programación"
