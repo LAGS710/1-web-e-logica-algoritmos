31b3370bb9d4480c454deb4a3aee1b7a9e46d903
LAGS710 <luisgonzales2_hotmail.com>
2025-04-18 15:40:23 -0600

    agregado de comments en el entregable 3

--- 03-metodos-arreglos/app.js
+++ 03-metodos-arreglos/app.js
@@ -6,12 +6,12 @@ const productos = [
     { nombre: "Celular", precio: 600, categoria: "Electrónica" },
 ];
 
-const filtro = productos.filter ( productos => productos.precio < 100)
+const filtro = productos.filter ( productos => productos.precio < 100); // Usa filter() para obtener los productos que cuesten menos de $100
 
-console.log(filtro)
+console.log(filtro) //Muestra los resultados de la aplicación de cada métiodo en consola.
 
-const ordernar = productos.sort((a,b) => a.nombre.localeCompare(b.nombre));
-console.log(ordernar)
+const ordernar = productos.sort((a,b) => a.nombre.localeCompare(b.nombre)); // Usa sort() para ordenar esos productos alfabéticamente por su nombre.
+console.log(ordernar) //Muestra los resultados de la aplicación de cada métiodo en consola.
 
-const nuevoArreglo = productos.map(productos => productos.nombre)
-console.log(nuevoArreglo)
+const nuevoArreglo = productos.map(productos => productos.nombre); //usa map() para generar un nuevo arreglo que contenga solo los nombres de los productos.
+console.log(nuevoArreglo) // Muestra los resultados de la aplicación de cada métiodo en consola.
